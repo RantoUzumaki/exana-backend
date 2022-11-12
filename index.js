d3116d6a2170cdb6ee3c335e07b103de505369ba
RantoUzumaki <rberk2011@gmail.com>
2022-11-12 21:50:25 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const express = require("express");
-var app = express();
+import mongoose from "mongoose";
+import express from "express";
+import * as dotenv from "dotenv";
+
+const ENV = dotenv.config();
+const app = express();
+
+const url = `mongodb+srv://${ENV.parsed.USERNAME_MONGODB}:${ENV.parsed.PASSWORD_MONGODB}@${ENV.parsed.CLUSTER_MONGODB}.ok0wnuj.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.get("/", function (request, response) {
   response.send("Hello World!");
